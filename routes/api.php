4dd4ce2d220379e1b072ad8a9635464a2686b389
Cleiton Lima <acrl.ribeiro17@gmail.com>
2021-09-20 16:25:50 -0300

    Congigurando as rotas da api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('cliente', 'ClienteController');
+Route::apiResource('pedido', 'PedidoController');
+Route::apiResource('produto', 'ProdutoController');
